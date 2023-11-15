1a8dbfb5506be75aae1019f0e07ab72bebb5a172
Mohammad Shapouri <mohammadshapouri02@gmail.com>
2023-11-15 13:19:35 +0330

    Added otp manager, otp serializer and otp view

--- config/otp/models.py
+++ config/otp/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from django.conf import settings
+from .otpConfig.otpConfig import OTPConfig
 # Create your models here.
 
 
 class OTPCode(models.Model):
     OTP_TYPE_CHOICES = (
         ('timer_counter_based', 'Both Timer and Counter Based'),
-        ('counter_based', 'Counter Based')
+        ('counter_based', 'Counter Based'),
+        ('timer_based', 'Timer Based')
     )
 
     OTP_USAGE_CHOICES = (
@@ -17,11 +20,13 @@ class OTPCode(models.Model):
     )
 
     user                = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='OTPCode', verbose_name='Related User')
+    otp                 = models.CharField(max_length=OTPConfig.OTPLength, blank=False, null=False, verbose_name="One Time Password Code")
     otp_type            = models.CharField(choices=OTP_TYPE_CHOICES, max_length=19, verbose_name='OTP Type')
     otp_usage           = models.CharField(choices=OTP_USAGE_CHOICES, max_length=29, verbose_name='OTP Usage')
     otp_creation_date   = models.DateTimeField(auto_now_add=True, verbose_name='Currrent OTP Code Creation Date')
     expire_after        = models.PositiveIntegerField(blank=True, null=True, verbose_name='Expiration Date')
     try_counter         = models.PositiveIntegerField(default=0, verbose_name='Number of Attempts')
+    max_possible_try    = models.PositiveIntegerField(default=3, verbose_name='Maximum possible number of Attempts')
     last_try            = models.DateTimeField(auto_now=True, verbose_name='Last Attempt')
     need_recreation     = models.BooleanField(default=False, verbose_name='Does User Need New OTP Code?')
 
@@ -33,3 +38,20 @@ class OTPCode(models.Model):
 
     def __str__(self):
         return str(self.user) + ' -- ' + str(self.otp_usage)
+    
+
+    def canTry(self):
+        if self.try_counter < self.max_possible_try:
+            return True
+        return False
+    
+
+    def hasTime(self):
+        if timezone.timedelta(seconds=self.otp_creation_date) + self.expire_after < timezone.now():
+            return True
+        return False
+
+
+    def incrementTryCounterValue(self):
+        self.try_counter += 1
+        self.save()
--- config/otp/views.py
+++ config/otp/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from .CustomOTPManager import CustomAccountVerificationOTPManager, CustomNewPhoneNumberVerificationOTPManager, CustomForgetPasswordOTPManager, CustomForgetPasswordOTPManager
+from .serializers import OTPVerifierSerializer
+from .models import OTPCode
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+
+UserModel = get_user_model()
+
+
+
+class validateAccountVerificationOTP(GenericAPIView, CustomAccountVerificationOTPManager):
+    permission_classes = [AllowAny]
+    serializer_class = OTPVerifierSerializer
+    queryset = OTPCode.objects.all()
+
+        
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        user = UserModel.objects.get(pk=validated_data['pk'])
+        return self.verifyOTP(user, 'timer_based', 'account_verification', validated_data['otp'])
+
+
+
+class validateNewPhoneNumberVerificationOTP(GenericAPIView, CustomNewPhoneNumberVerificationOTPManager):
+    permission_classes = [AllowAny]
+    serializer_class = OTPVerifierSerializer
+    queryset = OTPCode.objects.all()
+
+        
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        user = UserModel.objects.get(pk=validated_data['pk'])
+        return self.verifyOTP(user, 'new_phone_number_verification', validated_data['otp'])
+
