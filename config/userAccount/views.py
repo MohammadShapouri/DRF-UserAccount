936c7dcc8b95d22f3e18f121b8d66155904b82d4
Mohammad Shapouri <100878348+MohammadShapouri@users.noreply.github.com>
2023-11-24 15:31:10 +0330

    Update views.py

--- config/userAccount/views.py
+++ config/userAccount/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from .models import UserAccount
 from .permissions import ISOwnerOrAdmin
-from .serializers import UserAccountCreationSerializer, UserAccountUpdaterializer, UserAccountRetrivalSerializer, ChangePasswordSerializer, ResetPasswordSerializer, UserAccountDeletionSerializer
+from .serializers import UserAccountCreationSerializer, UserAccountUpdateSerializer, UserAccountRetrivalSerializer, ChangePasswordSerializer, ResetPasswordSerializer, UserAccountDeletionSerializer
 from django.db.models import Q
 from .userAccountOTPManager import UserAccountOTPManager, UserAccountVerificationOTPManager, UserAccountNewPhoneNumberVerificationOTPManager
 from rest_framework.response import Response
